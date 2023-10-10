d5ca17388abe3bcf3f9bbc7372d360f32c8c6f8b
Vadim Volkov <vvadim.vvolkov.ps4@gmail.com>
2023-10-10 20:43:19 +0600

    updated main method

--- 2 lab/main.py
+++ 2 lab/main.py
@@ -2,11 +2,17 @@
 # | 2 | 3 | 1 | 6 | 4 | 5 |
 from encrypt import encrypt
 from decrypt import decrypt
+
 file_path = r""
-key = [
+encryption_key = [
     [1, 2, 3, 4, 5, 6],
     [2, 3, 1, 6, 4, 5]
 ]
+decryption_key = [
+    [1, 2, 3, 4, 5, 6],
+    [3, 1, 2, 5, 6, 4]
+]
 
-encrypt(file_path, key)
-decrypt(file_path, key)
+
+#encrypt(file_path, encryption_key)
+decrypt(file_path, decryption_key)

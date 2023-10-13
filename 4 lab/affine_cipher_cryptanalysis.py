5d7157de9511fe5a78d19d6221c3d766af6f4472
Vadim Volkov <vvadim.vvolkov.ps4@gmail.com>
2023-10-13 13:56:37 +0600

    add coms; get_inverse_by_mod, _get_key removed

--- 4 lab/affine_cipher_cryptanalysis.py
+++ 4 lab/affine_cipher_cryptanalysis.py
@@ -1,14 +1,15 @@
 import sys
 from math import gcd as coprime
+from decrypt_with_key import _get_key
 
 
 def _make_cryptoanalysis_affine_cipher(text, encoding, mod,
                                        text_frequent_symbol_code1, text_frequent_symbol_code2,
                                        encoding_frequent_symbol_code1, encoding_frequent_symbol_code2):
     __decrypted_text = ''
-    __assumption_coefficients = _get_assumption_alpha_and_beta(mod, text_frequent_symbol_code1, text_frequent_symbol_code2,
-                                                               encoding_frequent_symbol_code1, encoding_frequent_symbol_code2)
-    if __assumption_coefficients[0] == -1:
+    __assumption_coefficients = _get_assumption_alpha_and_beta(mod, text_frequent_symbol_code1, text_frequent_symbol_code2,    # Вызывается функция, которая находит альфа и бета 
+                                                               encoding_frequent_symbol_code1, encoding_frequent_symbol_code2) # из системы уравнений, являющихся предположением 
+    if __assumption_coefficients[0] == -1:                                                                                      
         return 'Невозможно подобрать alpha и beta'
     for symbol in text:
         for i in range(len(encoding[0])):
@@ -19,29 +20,20 @@ def _make_cryptoanalysis_affine_cipher(text, encoding, mod,
                 __decrypted_text += __decrypted_symbol
                 break
     
-    #key = __get_key(__assumption_coefficients)
-    return __decrypted_text
+    key = _get_key(__assumption_coefficients, mod)
+    return __decrypted_text, key
 
-
-def _get_assumption_alpha_and_beta(mod, text_frequent_symbol_code1, text_frequent_symbol_code2,
-                                   encoding_frequent_symbol_code1, encoding_frequent_symbol_code2):
-    for alpha in range(mod):
+def _get_assumption_alpha_and_beta(mod, text_frequent_symbol_code1, text_frequent_symbol_code2,         # Функция, которая решает систему уравнений и находит афльфа и бета
+                                   encoding_frequent_symbol_code1, encoding_frequent_symbol_code2):     # Если система уравнений не решается, то возращает -1, которое дальше 
+    for alpha in range(mod):                                                                            # обрабатывается
         if coprime(alpha, mod) != 1:
             continue
         for beta in range(mod):
             if ((text_frequent_symbol_code1 * alpha + beta) % mod == encoding_frequent_symbol_code1) and (
                     (text_frequent_symbol_code2 * alpha + beta) % mod == encoding_frequent_symbol_code2):
                 return [alpha, beta]
-    #print('Невозможно подобрать ключ для расшифровки')
     return [-1]
 
 
-def _get_inverse_num_by_mod(num, mod):
-    for i in range(mod):
-        if (num * i) % mod == 1:
-            return i
 
-def __get_key(coefs, mod):
-    a = _get_inverse_num_by_mod(coefs[0], mod) % mod
-    b = ((-coefs[1]) * a) % mod
-    return [a, b]
+

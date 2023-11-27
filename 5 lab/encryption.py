ca6784edfc99bd6b8f5cb576004d60919024af38
Vadim Volkov <vvadim.vvolkov.ps4@gmail.com>
2023-11-27 20:03:45 +0600

    rewrite read_file_data func

--- 5 lab/encryption.py
+++ 5 lab/encryption.py
@@ -1,9 +1,9 @@
-from func_package import convert_hex_to_bin, wrap_file_bits_by_6, make_bin_from_rows_and_cols
+from func_package import wrap_file_bits_by_6, make_bin_from_rows_and_cols, convert_int_to_bin
 from func_package import get_s_block_row_and_column, get_row_and_value, wrap_str_by_8, convert_binary_to_decimal
 
 
 def encrypt(filename):
-    wrapped_bits_seq = wrap_file_bits_by_6(''.join(map(convert_hex_to_bin, read_file_data(filename))))
+    wrapped_bits_seq = wrap_file_bits_by_6(''.join(map(convert_int_to_bin, read_file_data(filename))))
     popped_data = wrapped_bits_seq.pop() if len(wrapped_bits_seq[-1]) < 6 else ''
     write_file_data(filename, list(
         map(convert_binary_to_decimal, wrap_str_by_8(''.join(map(make_bin_from_rows_and_cols,
@@ -11,14 +11,9 @@ def encrypt(filename):
 
 
 def read_file_data(filename):     # Чтение файла
-    file_code = []
     with open(filename, "rb") as file:
-        while True:
-            byte = file.read(1)
-            if not byte:
-                return file_code
-            else:
-                file_code.append(byte)
+        file_code = file.read()
+        return list(file_code)
 
 
 def write_file_data(filename, data):

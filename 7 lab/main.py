c47ac04a3f62a4ce6513a8d93b24160d2b97a976
Vadim Volkov <vvadim.vvolkov.ps4@gmail.com>
2023-11-29 00:03:00 +0600

    finish loop; call build_graph func

--- 7 lab/main.py
+++ 7 lab/main.py
@@ -1,9 +1,14 @@
-from func_package import prepare_text
+from func_package import prepare_text, count_entropy, build_graph
 
 
 iterations = 5
+H_outputs = []
 text = 'Разворачивайтесь в марше! Словесной не место кляузе. Тише, ораторы! Ваше слово, товарищ маузер. Довольно жить законом, данным Адамом и Евой. Клячу истории загоним. Левой! Левой! Левой!'
 modified_text = prepare_text(text)
 
 for k in range(1, iterations+1):
     k_grams_list = [''.join(text[i-k:i]) for i in range(k, len(text)+k,k)]
+    H_outputs.append(count_entropy(k_grams_list)/k)
+    print(f'k = {k}: E = {H_outputs[k-1]/k}')
+
+build_graph(iterations, H_outputs)
